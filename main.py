cec50508520197c342275fe85bf1856387c18f55
David Porter <david@porter.me>
2019-03-18 22:24:27 -0700

    refactor

--- main.py
+++ main.py
@@ -3,12 +3,8 @@ import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 
-# from keras.layers import Embedding
-
 print("Using TF version", tf.__version__)
 
-DATA_URL = "https://github.com/bobbypage/nlp-stocks/blob/master/stocknews/Combined_News_DJIA.csv?raw=true"
-# df = pd.read_csv(DATA_URL)
 import pdb
 
 
@@ -20,9 +16,10 @@ def loadDataCombinedColumns(DATA_URL):
     data = pd.read_csv(
         DATA_URL, parse_dates=True, index_col=0, verbose=True, keep_default_na=False
     )
+    data["combined_news"] = data.filter(regex=("Top.*")).apply(lambda x: ''.join(str(x.values)), axis=1)
     data_y = data["Label"]
     data_X = data.iloc[:, 1:26]
-    #     data_X = data.iloc[:,1:26].apply(lambda headline:cleanString('BLAH '.join(headline)), axis=1)
+        # data_X = data.iloc[:,1:26].apply(lambda headline:cleanString('BLAH '.join(headline)), axis=1)
 
     test_X = data_X["2015-01-02":"2016-07-01"]
     train_X = data_X["2008-08-08":"2014-12-31"]
@@ -42,29 +39,7 @@ def mergeNews(x_df, y_df):
             new_y.extend([y_label] * len(news.values))
     return new_x, new_y
 
-train_x, train_y, test_x, test_y = loadDataCombinedColumns("./stocknews/Combined_News_DJIA.csv")
-
-cleaned_train_x, cleaned_train_y = mergeNews(train_x, train_y)
-
-print(len(cleaned_train_x), len(cleaned_train_y))
-
-cleaned_test_x, cleaned_test_y = mergeNews(test_x, test_y)
-
-
-# get the longest new article length
-max_length = len(max(cleaned_train_x, key=len))
-print("Max length", max_length)
-print("Number of news training examples", len(cleaned_train_x))
-
-
-# initialize the tokenizer
-tokenizer = tf.keras.preprocessing.text.Tokenizer()
-
-tokenizer.fit_on_texts(cleaned_train_x)
-vocab_size = len(tokenizer.word_index) + 1
-
-
-def encode_and_pad_data(data, tokenizer):
+def encode_and_pad_data(data, tokenizer, max_length):
     # integer encode the text data
     encoded_data = tokenizer.texts_to_sequences(data)
 
@@ -78,74 +53,105 @@ def encode_and_pad_data(data, tokenizer):
     return padded_data
 
 
-train_x_encoded = encode_and_pad_data(cleaned_train_x, tokenizer)
-test_x_encoded = encode_and_pad_data(cleaned_test_x, tokenizer)
+def create_glove_embeddings():
+
+    # Load the GLOVE embeddings
+    embeddings_index = {}
+    with open("glove/glove.6B.100d.txt", encoding="utf-8") as f:
+        for line in f:
+            values = line.split(" ")
+            word = values[0]
+            embedding = np.asarray(values[1:], dtype="float32")
+            embeddings_index[word] = embedding
+
+    print("Word embeddings:", len(embeddings_index))
+    return embeddings_index
 
 
-# Load the GLOVE embeddings
-
-# Load GloVe's embeddings
-embeddings_index = {}
-with open("glove/glove.6B.100d.txt", encoding="utf-8") as f:
-    for line in f:
-        values = line.split(" ")
-        word = values[0]
-        embedding = np.asarray(values[1:], dtype="float32")
-        embeddings_index[word] = embedding
-
-print("Word embeddings:", len(embeddings_index))
-EMBEDDING_DIM = 100
 
 
-# Create the embedding matrix
+def main():
+    EMBEDDING_DIM = 100
 
-embedding_matrix = np.zeros((vocab_size, EMBEDDING_DIM))
-for word, i in tokenizer.word_index.items():
-    embedding_vector = embeddings_index.get(word)
-    if embedding_vector is not None:
-        # words not found in embedding index will be all-zeros.
-        embedding_matrix[i] = embedding_vector
+    train_x, train_y, test_x, test_y = loadDataCombinedColumns("./stocknews/Combined_News_DJIA.csv")
+    cleaned_train_x, cleaned_train_y = mergeNews(train_x, train_y)
+    cleaned_test_x, cleaned_test_y = mergeNews(test_x, test_y)
 
-e = tf.keras.layers.Embedding(
-    len(tokenizer.word_index) + 1,
-    EMBEDDING_DIM,
-    weights=[embedding_matrix],
-    input_length=max_length,
-    trainable=False,
-)
-
-model = tf.keras.Sequential()
-model.add(e)
-model.add(tf.keras.layers.GlobalMaxPool1D())
-model.add(tf.keras.layers.Dense(10, activation="relu"))
-model.add(tf.keras.layers.Dense(1, activation="sigmoid"))
-model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
-model.summary()
+    print("Length of data")
+    print(len(cleaned_train_x), len(cleaned_train_y))
 
 
-# print(train_x_encoded.shape)
-# print(np.arraycleaned_train_y))
+    # get the longest new article length
+    max_length = len(max(cleaned_train_x, key=len))
+    print("Max length", max_length)
+    print("Number of news training examples", len(cleaned_train_x))
 
 
-train_x_np = np.array(train_x_encoded)
-train_y_np = np.array(cleaned_train_y)
+    # initialize the tokenizer
+    tokenizer = tf.keras.preprocessing.text.Tokenizer()
 
-test_x_np = np.array(test_x_encoded)
-test_y_np = np.array(cleaned_test_y)
+    tokenizer.fit_on_texts(cleaned_train_x)
+    vocab_size = len(tokenizer.word_index) + 1
 
-print(train_x_np.shape)
-print(train_y_np.shape)
+    train_x_encoded = encode_and_pad_data(cleaned_train_x, tokenizer, max_length)
+    test_x_encoded = encode_and_pad_data(cleaned_test_x, tokenizer, max_length)
 
-# print(train_x_encoded[0])
-history = model.fit(
-    train_x_np,
-    train_y_np,
-    epochs=50,
-    verbose=1,
-    validation_data=(test_x_np, test_y_np),
-    batch_size=10,
-)
-# loss, accuracy = model.evaluate(X_train, y_train, verbose=False)
-# print("Training Accuracy: {:.4f}".format(accuracy))
-# loss, accuracy = model.evaluate(X_test, y_test, verbose=False)
-# print("Testing Accuracy:  {:.4f}".format(accuracy))
+    embeddings_index = create_glove_embeddings()
+
+    # Create the embedding matrix
+
+    embedding_matrix = np.zeros((vocab_size, EMBEDDING_DIM))
+    for word, i in tokenizer.word_index.items():
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            # words not found in embedding index will be all-zeros.
+            embedding_matrix[i] = embedding_vector
+
+    e = tf.keras.layers.Embedding(
+        len(tokenizer.word_index) + 1,
+        EMBEDDING_DIM,
+        weights=[embedding_matrix],
+        input_length=max_length,
+        trainable=False,
+    )
+
+    model = tf.keras.Sequential()
+    model.add(e)
+    model.add(tf.keras.layers.GlobalMaxPool1D())
+    model.add(tf.keras.layers.Dense(10, activation="relu"))
+    model.add(tf.keras.layers.Dense(1, activation="sigmoid"))
+    model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+    model.summary()
+
+
+    # print(train_x_encoded.shape)
+    # print(np.arraycleaned_train_y))
+
+
+    train_x_np = np.array(train_x_encoded)
+    train_y_np = np.array(cleaned_train_y)
+
+    test_x_np = np.array(test_x_encoded)
+    test_y_np = np.array(cleaned_test_y)
+
+    print(train_x_np.shape)
+    print(train_y_np.shape)
+
+    # print(train_x_encoded[0])
+    history = model.fit(
+        train_x_np,
+        train_y_np,
+        epochs=50,
+        verbose=1,
+        validation_data=(test_x_np, test_y_np),
+        batch_size=10,
+    )
+    # loss, accuracy = model.evaluate(X_train, y_train, verbose=False)
+    # print("Training Accuracy: {:.4f}".format(accuracy))
+    # loss, accuracy = model.evaluate(X_test, y_test, verbose=False)
+    # print("Testing Accuracy:  {:.4f}".format(accuracy))
+
+
+
+if __name__ == "__main__":
+    main()
